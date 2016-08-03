a52d9fb995d694047cc92daf7ea0c98781108149
Christian Aistleitner <christian@quelltextlich.at>
2016-08-03 14:55:16 -0700

    Add add-project action
    Change-Id: Ifc687456ba58e0521bf6bcb0cd54fdda6412303a

---
+++ src/main/java/com/googlesource/gerrit/plugins/its/phabricator/conduit/results/QueryResult.java
@@ -0,0 +1,50 @@
+//Copyright (C) 2015 The Android Open Source Project
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+package com.googlesource.gerrit.plugins.its.phabricator.conduit.results;
+
+import com.google.gson.JsonElement;
+
+/**
+ * Models the result for API methods returning a (possible paged) QueryResult
+ * <p/>
+ * JSON looks like:
+ * <pre>
+ * {
+ *   "data": { ... },
+ *   "slugMap": [],
+ *   "cursor": {
+ *     "limit": 100,
+ *     "after": null,
+ *     "before": null
+ *   }
+ * }
+   * </pre>
+   */
+public class QueryResult {
+  private JsonElement data;
+  private JsonElement slugMap;
+  private JsonElement cursor;
+
+  public JsonElement getData() {
+    return data;
+  }
+
+  public JsonElement getSlugMap() {
+    return slugMap;
+  }
+
+  public JsonElement getCursor() {
+    return cursor;
+  }
+}
