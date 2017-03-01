4ad5481de17dfa732cd7bace9e8f9f1d3a3487b4
Paladox none <thomasmulhall410@yahoo.com>
2017-03-01 21:00:20 +0000

    Migrate this plugin to use maniphest.search conduit
    maniphest.info is deprecated and can be removed at anytime.
    
    Our downstream task https://phabricator.wikimedia.org/T159041
    
    Change-Id: I228bf25aa4fc93b20989a03bdd2ff83ce69ba9ca

--- src/main/java/com/googlesource/gerrit/plugins/its/phabricator/PhabricatorItsFacade.java
+++ src/main/java/com/googlesource/gerrit/plugins/its/phabricator/PhabricatorItsFacade.java
@@ -32,7 +32,7 @@ import com.googlesource.gerrit.plugins.its.base.its.ItsFacade;
 import com.googlesource.gerrit.plugins.its.phabricator.conduit.Conduit;
 import com.googlesource.gerrit.plugins.its.phabricator.conduit.ConduitErrorException;
 import com.googlesource.gerrit.plugins.its.phabricator.conduit.ConduitException;
-import com.googlesource.gerrit.plugins.its.phabricator.conduit.results.ManiphestInfo;
+import com.googlesource.gerrit.plugins.its.phabricator.conduit.results.ManiphestSearch;
 import com.googlesource.gerrit.plugins.its.phabricator.conduit.results.ProjectInfo;
 
 public class PhabricatorItsFacade implements ItsFacade {
@@ -81,7 +81,7 @@ public class PhabricatorItsFacade implements ItsFacade {
     int task_id = Integer.parseInt(bugId);
     try {
       try {
-        conduit.maniphestInfo(task_id);
+        conduit.maniphestSearch(task_id);
         ret = true;
       } catch (ConduitErrorException e) {
         // An ERR_BAD_TASK just means that the task does not exist.
@@ -139,7 +139,7 @@ public class PhabricatorItsFacade implements ItsFacade {
 
       Set<String> projectPhids = Sets.newHashSet(projectPhid);
 
-      ManiphestInfo taskInfo = conduit.maniphestInfo(taskId);
+      ManiphestSearch taskInfo = conduit.maniphestSearch(taskId);
       for (JsonElement jsonElement :
         taskInfo.getProjectPHIDs().getAsJsonArray()) {
         projectPhids.add(jsonElement.getAsString());
