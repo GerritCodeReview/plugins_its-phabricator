c6bb2d03175017db1e5a642b008b8df00d0960e3
Christian Aistleitner <christian@quelltextlich.at>
2020-05-19 11:18:23 +0200

    Make implicit expectations about Conduit explicit in Facate tests
    Change-Id: I5287019a3370070fded495436e18b24ffff01320

--- src/test/java/com/googlesource/gerrit/plugins/its/phabricator/PhabricatorItsFacadeTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/its/phabricator/PhabricatorItsFacadeTest.java
@@ -15,6 +15,7 @@ package com.googlesource.gerrit.plugins.its.phabricator;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import com.google.gerrit.extensions.annotations.PluginName;
@@ -30,60 +31,65 @@ import org.junit.Test;
 public class PhabricatorItsFacadeTest extends LoggingMockingTestCase {
   private Injector injector;
   private Config serverConfig;
+  private Conduit conduit;
   private Conduit.Factory conduitFactory;
 
   @Test
   public void testCreateLinkForWebUiDifferentUrlAndText() {
-    mockUnconnectablePhabricator();
-
     PhabricatorItsFacade itsFacade = createPhabricatorItsFacade();
     String actual = itsFacade.createLinkForWebui("Test-Url", "Test-Text");
 
     assertThat(actual).isEqualTo("[[Test-Url|Test-Text]]");
+
+    verifyZeroInteractions(conduit);
   }
 
   @Test
   public void testCreateLinkForWebUiSameUrlAndText() {
-    mockUnconnectablePhabricator();
-
     PhabricatorItsFacade itsFacade = createPhabricatorItsFacade();
     String actual = itsFacade.createLinkForWebui("Test-Url", "Test-Url");
 
     assertThat(actual).isEqualTo("[[Test-Url]]");
+
+    verifyZeroInteractions(conduit);
   }
 
   @Test
   public void testCreateLinkForWebUiNullText() {
-    mockUnconnectablePhabricator();
-
     PhabricatorItsFacade itsFacade = createPhabricatorItsFacade();
     String actual = itsFacade.createLinkForWebui("Test-Url", null);
 
     assertThat(actual).isEqualTo("[[Test-Url]]");
+
+    verifyZeroInteractions(conduit);
   }
 
   @Test
   public void testCreateLinkForWebUiEmptyText() {
-    mockUnconnectablePhabricator();
-
     PhabricatorItsFacade itsFacade = createPhabricatorItsFacade();
     String actual = itsFacade.createLinkForWebui("Test-Url", "");
 
     assertThat(actual).isEqualTo("[[Test-Url]]");
+
+    verifyZeroInteractions(conduit);
   }
 
   private PhabricatorItsFacade createPhabricatorItsFacade() {
     return injector.getInstance(PhabricatorItsFacade.class);
   }
 
-  private void mockUnconnectablePhabricator() {
-    when(serverConfig.getString("its-phabricator", null, "url")).thenReturn("<no-url>");
-    when(serverConfig.getString("its-phabricator", null, "token")).thenReturn("none");
-  }
-
   @Override
   public void setUp() throws Exception {
     super.setUp();
+
+    serverConfig = mock(Config.class);
+    conduitFactory = mock(Conduit.Factory.class);
+    conduit = mock(Conduit.class);
+
+    when(serverConfig.getString("its-phabricator", null, "url"))
+        .thenReturn("http://phab.example.org/");
+    when(serverConfig.getString("its-phabricator", null, "token")).thenReturn("cli-FOO");
+    when(conduitFactory.create("http://phab.example.org/", "cli-FOO")).thenReturn(conduit);
 
     injector = Guice.createInjector(new TestModule());
   }
@@ -91,8 +97,6 @@ public class PhabricatorItsFacadeTest extends LoggingMockingTestCase {
   private class TestModule extends FactoryModule {
     @Override
     protected void configure() {
-      serverConfig = mock(Config.class);
-      conduitFactory = mock(Conduit.Factory.class);
       bind(Config.class).annotatedWith(GerritServerConfig.class).toInstance(serverConfig);
       bind(String.class).annotatedWith(PluginName.class).toInstance("its-phabricator");
       bind(Conduit.Factory.class).toInstance(conduitFactory);
